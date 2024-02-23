6bbf216235080d6422f73ae65aae7d74e6e01734
Pohan <oange6214@gmail.com>
2024-02-23 22:41:28 +0800

    feat: 增加 MongoDB.Driver Package feat: 建立 Driver Model 與 MongoDB Mapping feat: 增加 MongoDb 配置（更新 appsettings.json） feat: 增加服務（在 Program.cs 中）

--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,9 @@
+using DriversAppApi.Configurations;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection("MongoDatabase"));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
