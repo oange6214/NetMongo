8c0740d9df2e5f3c095089312b0a5deccd7b5349
Pohan <oange6214@gmail.com>
2024-02-23 23:34:10 +0800

    feat. 建立 DriverService 資料庫存取邏輯，並 Programe.cs 進行注入

--- src/Program.cs
+++ src/Program.cs
@@ -1,9 +1,12 @@
 using DriversAppApi.Configurations;
+using DriversAppApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection("MongoDatabase"));
+
+builder.Services.AddSingleton<DriverService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
