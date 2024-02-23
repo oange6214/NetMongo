5c9c0c6ca1e379ccb4ef045a74d725d6388e5c76
Pohan <oange6214@gmail.com>
2024-02-24 01:05:27 +0800

    feat: 新增 DriversController 的 Get、Post、Put 與 Delete 功能
    這次提交新增了 DriversController 中的四個 HTTP endpoints：
    - Get 方法用於檢索所有駕駛員資訊。
    - Post 方法用於建立新的駕駛員記錄。
    - Put 方法用於更新現有的駕駛員記錄。
    - Delete 方法用於刪除指定的駕駛員記錄。

--- src/Controllers/DriversController.cs
+++ src/Controllers/DriversController.cs
@@ -1,3 +1,4 @@
+using DriversAppApi.Models;
 using DriversAppApi.Services;
 using Microsoft.AspNetCore.Mvc;
 
@@ -21,5 +22,51 @@ public class DriversController : ControllerBase
             return NotFound();
 
         return Ok(existingDriver);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        var allDrivers = await _driverService.GetAsync();
+
+        if (allDrivers.Any())
+            return Ok(allDrivers);
+
+        return NotFound();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Post(Driver driver)
+    {
+        await _driverService.CreateAsync(driver);
+        return CreatedAtAction(nameof(Get), new { id = driver.Id }, driver);
+    }
+
+    [HttpPut("{id:length(24)}")]
+    public async Task<IActionResult> Update(string id, Driver driver)
+    {
+        var existingDriver = await _driverService.GetAsync(id);
+
+        if (existingDriver is null)
+            return BadRequest();
+
+        driver.Id = existingDriver.Id;
+
+        await _driverService.UpdateAsync(driver);
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id:length(24)}")]
+    public async Task<IActionResult> Delete(string id)
+    {
+        var existingDriver = await _driverService.GetAsync(id);
+
+        if (existingDriver is null)
+            return BadRequest();
+
+        await _driverService.RemoveAsync(id);
+
+        return NoContent();
     }
 }
